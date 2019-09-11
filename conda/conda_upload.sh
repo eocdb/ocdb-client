1d95dfe8f77a110276e75cce370b7ba61fcf611a
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-09-11 17:07:54 +0200

    corrected anaconda upload

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -1,10 +1,10 @@
 # Only need to change these two variables
 PKG_NAME=ocdb
-USER=ocdb
+USER=ocdb-client
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=0.1.1
 conda build recipe
-anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-0.tar.bz2 --force
+anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-py0.tar.bz2 --force
