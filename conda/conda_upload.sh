0261615f6c21467ccbce52e6922b5e36823f579c
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-09-18 14:10:50 +0200

    attempty to fix syntax error

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -7,6 +7,7 @@ conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=0.2.1
 conda build recipe
+
 echo anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-py_0.tar.bz2 --force
 
 anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-py_0.tar.bz2 --force
