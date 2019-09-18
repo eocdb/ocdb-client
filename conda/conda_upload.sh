3dc922eef673dcd523cbc7773904f95a6e571483
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-09-18 14:06:36 +0200

    attempty to fix syntax error

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -1,10 +1,12 @@
 # Only need to change these two variables
-PKG_NAME=ocdb-client
-USER=ocdb
+export PKG_NAME=ocdb-client
+export USER=ocdb
 
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
 export VERSION=0.2.1
 conda build recipe
+echo anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-py_0.tar.bz2 --force
+
 anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-py_0.tar.bz2 --force
