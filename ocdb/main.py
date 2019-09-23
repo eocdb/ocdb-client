a60ffb1fae8c52204776b55310435b61408fe011
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-09-23 17:43:55 +0200

    - Fixed submission download option bug - Added profiling

--- ocdb/main.py
+++ ocdb/main.py
@@ -1,5 +1,15 @@
+import cProfile
+import io
+import pstats
 from ocdb.api import new_api
 from ocdb.cli import cli
+
+
+_PROFILING_ON = True
+
+if _PROFILING_ON:
+    pr = cProfile.Profile()
+    pr.enable()
 
 
 def main(args=None):
@@ -8,3 +18,12 @@ def main(args=None):
 
 if __name__ == '__main__':
     main()
+
+
+if _PROFILING_ON:
+    # noinspection PyUnboundLocalVariable
+    pr.disable()
+    s = io.StringIO()
+    ps = pstats.Stats(pr, stream=s).sort_stats('cumtime')
+    ps.print_stats()
+    print(s.getvalue())
