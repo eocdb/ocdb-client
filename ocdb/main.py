ebe260eb5f07d66effb254b567c0c680179c9305
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-09-23 17:47:03 +0200

    removed profiler

--- ocdb/main.py
+++ ocdb/main.py
@@ -1,15 +1,5 @@
-import cProfile
-import io
-import pstats
 from ocdb.api import new_api
 from ocdb.cli import cli
-
-
-_PROFILING_ON = True
-
-if _PROFILING_ON:
-    pr = cProfile.Profile()
-    pr.enable()
 
 
 def main(args=None):
@@ -19,11 +9,3 @@ def main(args=None):
 if __name__ == '__main__':
     main()
 
-
-if _PROFILING_ON:
-    # noinspection PyUnboundLocalVariable
-    pr.disable()
-    s = io.StringIO()
-    ps = pstats.Stats(pr, stream=s).sort_stats('cumtime')
-    ps.print_stats()
-    print(s.getvalue())
