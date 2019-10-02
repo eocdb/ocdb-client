d458efc396954decfc8299f3fec3be6d6b51e269
Helge Dzierzon <helge.dzierzon@brockmann-consult.de>
2019-10-02 10:06:23 +0200

    Corrected version in meta.yaml and upload

--- conda/conda_upload.sh
+++ conda/conda_upload.sh
@@ -5,7 +5,7 @@ export USER=ocdb
 mkdir ~/conda-bld
 conda config --set anaconda_upload no
 export CONDA_BLD_PATH=~/conda-bld
-export VERSION=0.2.2
+export VERSION=0.2.3
 conda build recipe
 
 echo anaconda -t ${CONDA_UPLOAD_TOKEN} upload  -u ${USER} ${CONDA_BLD_PATH}/noarch/${PKG_NAME}-${VERSION}-py_0.tar.bz2 --force
