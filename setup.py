c006f621f8b02196a1bf081b17e93d0861769bc7
Ilaria Cazzaniga <ilaria.cazzaniga@eumetsat.int>
2020-05-28 12:24:55 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 # The MIT License (MIT)
-# Copyright (c) 2018 by EUMETSAT
+# Copyright (c) 2019 by Copernicus/EUMETSAT
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy of
 # this software and associated documentation files (the "Software"), to deal in
