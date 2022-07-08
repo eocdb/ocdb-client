45ebe5da7dd5590ac91620b91f344fae949f4800
sabine <sabine.embacher@brockmann-consult.de>
2022-07-08 12:02:11 +0200

    change to development version

--- ocdb/version.py
+++ ocdb/version.py
@@ -19,7 +19,7 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
-VERSION = "0.2.dev11"
+VERSION = "0.2.11dev"
 API_VERSION_TAG = "latest"
 DESCRIPTION = "Copernicus Ocean Colour In-Situ Database Python Client"
 DOCS_URL = 'https://ocdb.readthedocs.io/en/latest'
